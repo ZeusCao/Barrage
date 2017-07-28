c65b4d4b6d53f982e738e711c5122465f8f6fca2
曹檑 <1090491594@qq.com>
2017-07-28 17:26:53 +0800

    Initial Commit

---
+++ DanMuTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DanMuTest
+//
+//  Created by Zeus on 2017/7/28.
+//  Copyright © 2017年 Zeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
